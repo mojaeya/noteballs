6ea7dad326373a7ad3cdc61647293b40024fcbef
mojaeya <mojaeya@gmail.com>
2023-05-20 00:16:26 +0900

    feat: bulma css & design

---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div v-for="i in 3" class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          recusandae magni fugit assumenda quis quae architecto atque nihil
+          cupiditate delectus quisquam, quidem, quos commodi voluptatum,
+          consequatur quia velit suscipit illum?
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
+</template>
